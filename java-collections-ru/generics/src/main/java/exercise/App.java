1e9709beca1486c2ea098fdae3cbf0503112e5f4
@hexlet/cli <support@hexlet.io>
2023-12-13 18:36:16 +0600

    submit java-collections-ru/generics

--- java-collections-ru/generics/src/main/java/exercise/App.java
+++ java-collections-ru/generics/src/main/java/exercise/App.java
@@ -3,25 +3,9 @@ package exercise;
 import java.util.Map;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.HashMap;
 
 // BEGIN
 class App {
-    public static void main(String[] args) {
-        List<Map<String, String>> books = new ArrayList<>();
-        Map<String, String> book1 = new HashMap<>(Map.of("title", "Cymbeline",
-                "author", "Shakespeare", "year", "1611"));
-        Map<String, String> book2 = new HashMap<>(Map.of("title", "Book of Fooos",
-                "author", "FooBar", "year", "1111"));
-        Map<String, String> book3 = new HashMap<>(Map.of("title", "The Tempest",
-                "author", "Shakespeare", "year", "1611"));
-        Map<String, String> where = new HashMap<>(Map.of("author", "Shakespeare", "year", "1611"));
-        books.add(book1);
-        books.add(book2);
-        books.add(book3);
-        findWhere(books, where);
-    }
-
     public static List<Map<String, String>> findWhere(List<Map<String, String>> books, Map<String, String> where) {
         List<Map<String, String>> result = new ArrayList<>();
         for (Map<String, String> item: books) {
