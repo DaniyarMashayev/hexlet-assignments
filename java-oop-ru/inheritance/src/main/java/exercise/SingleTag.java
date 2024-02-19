578928f1a8b31e438a1940fa9e8da3d4edfb9a8e
@hexlet/cli <support@hexlet.io>
2024-02-20 00:16:48 +0600

    submit java-oop-ru/inheritance

---
+++ java-oop-ru/inheritance/src/main/java/exercise/SingleTag.java
@@ -0,0 +1,19 @@
+package exercise;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+// BEGIN
+public class SingleTag extends Tag {
+
+    public SingleTag(String tagName, Map<String, String> map) {
+        super(tagName, map);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
+// END
